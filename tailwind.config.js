3cd0deda2b7fb9bb111093319c64a399ea14a18e
Mikeclassic <120394575+Mikeclassic@users.noreply.github.com>
2025-11-29 11:00:52 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-content: [
-  "./app/**/*.{js,ts,jsx,tsx,mdx}", 
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./lib/**/*.{js,ts,jsx,tsx,mdx}"
-],
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // This adds the missing 'primary' color (using standard Blue)
+        primary: {
+          500: "#3B82F6",
+          600: "#2563EB",
+        },
       },
     },
   },
   plugins: [],
-}
+};
+export default config;
